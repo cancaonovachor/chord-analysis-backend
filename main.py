be9823500ca4b1c793c5e018cd2f23d7f97f5758
sunakane <20nm728a@vc.ibaraki.ac.jp>
2021-08-19 22:06:43 +0900

    cloud storageと連携して、送られたファイルに対してコードを付与するようにした

--- main.py
+++ main.py
@@ -1,29 +1,115 @@
 import os
-from flask import Flask, request, make_response, jsonify
-from musicxml_chord_analysis import analyze
+from flask import Flask, request, make_response, jsonify, flash, redirect, url_for, session
+from werkzeug.utils import secure_filename
+from google.cloud import storage
+
+from musicxml_chord_analysis import getChordMinimumUnit, writeChord
+from music21 import environment
+
+from io import StringIO
+
+environment.set('autoDownload', 'allow')
 
 app = Flask(__name__)
 
 UPLOAD_DIR = os.getenv("UPLOAD_DIR_PATH")
 
-@app.route('/upload', methods=['POST'])
-def upload_multipart():
-    if 'uploadFile' not in request.files:
-        make_response(jsonify({'result': 'uploadFile is required.'}))
+CLOUD_STORAGE_BUCKET = os.getenv("CLOUD_STORAGE_BUCKET")
 
-    file = request.files['uploadFile']
-    filename = file.filename
-    if '' == filename:
-        make_response(jsonify({'result': 'filename must not empty.'}))
+CLOUD_STORAGE_ENDPOINT = 'http://' + \
+    CROUD_STORAGE_BUCKET + '.storage.googleapis.com'
 
-    analyze(filename)
-    return make_response(jsonify({'result': filename}))
+# ファイルをアップロードして、ファイルへのURLを返す
+
+
+def upload_blob(source_file_object, destination_blob_name):
+    storage_client = storage.Client(
+        project_id, credentials=credential)
+    bucket = storage_client.bucket(CROUD_STORAGE_BUCKET)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_file(source_file_object, rewind=True)
+
+    print(
+        "File uploaded to {}".format(
+            destination_blob_name
+        )
+    )
+
+    # upload_from_fileがストリームの位置を変更するので、最初に戻す
+    source_file_object.seek(0)
+
+    return CLOUD_STORAGE_ENDPOINT + '/' + destination_blob_name
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        if 'uploadFile' not in request.files:
+            make_response(jsonify({'result': 'uploadFile is required.'}))
+
+        file = request.files.get('uploadFile')
+        filename = file.filename
+        if '' == filename:
+            make_response(jsonify({'result': 'filename must not empty.'}))
+
+        gcs = storage.Client()
+
+        bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+        blob = bucket.blob(filename)
+        blob.upload_from_string(
+            file.read(),
+            content_type=file.content_type
+        )
+
+        if file and allowed_file(file.filename):
+            # file: werzurg.FileStorage
+            filename = secure_filename(file.filename)
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for())
+            response = make_response()
+
+            # upload file to cloud storage
+            file_url = upload_blob(file, filename)
+
+            # file_url = cloud_storage_endpoint + '/nippon.musicxml'
+
+            chord_list = getChordMinimumUnit(
+                file_url, head=1, tail=-1, sameChordPass=1)
+
+            # 受け取ったmusicxmlに、オンメモリでコードを書き込む
+            output = StringIO()
+            output.write(writeChord(file, chord_list, head=1, tail=-1))
+
+            response.data = output.getvalue()
+
+            output.close()
+
+            downloadFileName = 'down' + filename
+            response.headers['Content-Disposition'] = 'attachment; filename=' + \
+                downloadFileName
+
+            response.mimetype = 'text/xml'
+
+            return response
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
